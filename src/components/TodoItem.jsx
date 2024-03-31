22bee9dcfaa42ace807cb1aaa8fb45a99e045e22
glennlxk <81400496+glennlxk@users.noreply.github.com>
2024-03-31 17:59:36 +0800

    Adding functionality to delete button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,20 @@
 import styles from "./todoitem.module.css";
 
-export default function TodoItem({ item }) {
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    setTodos(todos.filter((todo) => todo !== item));
+  }
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
         {item}
         <span>
-          <button className={styles.deleteButton}>X</button>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            X
+          </button>
         </span>
       </div>
       <hr className={styles.line} />
