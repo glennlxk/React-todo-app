c6a18384299d5efc458e196c5bcd83a3bca7e841
glennlxk <81400496+glennlxk@users.noreply.github.com>
2024-04-04 17:02:34 +0800

    Adding the line through function to completed todo items

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,10 +4,20 @@ export default function TodoItem({ item, todos, setTodos }) {
   function handleDelete(item) {
     setTodos(todos.filter((todo) => todo !== item));
   }
+  function handleClick(name) {
+    const newTodos = todos.map((todo) =>
+      todo.name === name ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(newTodos);
+  }
+  let completed = item.done ? styles.completed : "";
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item.name}
+        <span className={completed} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
+
         <span>
           <button
             onClick={() => handleDelete(item)}
