fff125cbac190e92194c4205ab6d4de4b4008472
glennlxk <81400496+glennlxk@users.noreply.github.com>
2024-04-04 21:24:46 +0800

    Add sorting function to sort todo list by completed and incompleted items

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,9 +2,12 @@ import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css";
 
 export default function TodoList({ todos, setTodos }) {
+  const sortedTodo = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className={styles.list}>
-      {todos.map((item) => (
+      {sortedTodo.map((item) => (
         <TodoItem
           key={item.name}
           item={item}
